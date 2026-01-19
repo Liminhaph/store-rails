6731cfed168e93321c00d62231e4851d067bcc53
Liminhaph <pedrohqlimaa@gmail.com>
2026-01-18 23:44:46 -0300

    Add user roles and admin authorization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,43 +1,34 @@
 class ProductsController < ApplicationController
-  # Garante que apenas usuários autenticados possam acessar as ações
   before_action :authenticate_user!
+  before_action :require_admin!, except: [:index, :show]
 
-  # Lista todos os produtos (gelos)
   def index
     @products = Product.all
   end
 
-  # Exibe um produto específico
   def show
     @product = Product.find(params[:id])
   end
 
-  # Formulário para criar um novo produto
   def new
     @product = Product.new
   end
 
-  # Cria um novo produto no banco
   def create
     @product = Product.new(product_params)
 
     if @product.save
-      # Envia e-mail notificando novo produto cadastrado
       ProductMailer.new_product(@product).deliver_later
-
       redirect_to @product
     else
-      # Reexibe o formulário caso haja erro de validação
       render :new, status: :unprocessable_entity
     end
   end
 
-  # Formulário de edição do produto
   def edit
     @product = Product.find(params[:id])
   end
 
-  # Atualiza os dados do produto
   def update
     @product = Product.find(params[:id])
 
@@ -48,24 +39,28 @@ class ProductsController < ApplicationController
     end
   end
 
-  # Remove o produto do sistema
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-
     redirect_to products_path
   end
 
   private
 
-  # Strong Parameters:
-  # Define quais campos podem ser recebidos via formulário
+  def require_admin!
+    unless current_user.admin?
+      redirect_to products_path, alert: "Você não tem permissão para realizar essa ação."
+    end
+  end
+
   def product_params
     params.require(:product).permit(
       :name,
       :description,
       :price,
-      :ice_type
+      :ice_type,
+      :in_stock
     )
   end
 end
+
